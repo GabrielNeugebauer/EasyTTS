f07a2b655f3768f198734511b05594653372d357
Gabriel Neugebauer <gabrielneugebauer@hotmail.com>
2025-05-09 15:02:33 -0300

    added try/except

--- EasyTTS.py
+++ EasyTTS.py
@@ -3,24 +3,28 @@ import asyncio
 import os
 
 async def main():
-    print('Bem vindo ao EasyTTS!\nDeveloped by @gabrielneugebauer\n\nDigite o caminho do arquivo de texto que deseja converter em áudio:')
-    path = input().replace("\\", "/").replace("'", "").replace('"', "")
-    texto = loadFile(path)
-    texto = texto = ', '.join(texto).replace(".,",".")
-    if not texto:
-        print("[!] O arquivo está vazio ou não foi encontrado.")
+    try:
+        print('Bem vindo ao EasyTTS!\nDeveloped by @gabrielneugebauer\n\nDigite o caminho do arquivo de texto que deseja converter em áudio:')
+        path = input().replace("\\", "/").replace("'", "").replace('"', "")
+        texto = loadFile(path)
+        texto = texto = ', '.join(texto).replace(".,",".")
+        if not texto:
+            print("[!] O arquivo está vazio ou não foi encontrado.")
+            return
+        print('Digite o nome do arquivo de áudio que deseja salvar (sem extensão):')
+        nome_arquivo = input()
+        print (f"{path}")
+        # print (texto) # Para debug
+        comunicador = edge_tts.Communicate(
+            text=texto,
+            voice="pt-BR-AntonioNeural",  # Ou "pt-BR-FranciscaNeural"
+            rate="+5%",  # velocidade: -100% a +100%
+        )
+        await comunicador.save(f"Resultados\\{nome_arquivo}.mp3")
+        print(f"Arquivo de áudio gerado com sucesso em {os.getcwd()}\\Resultados\\{nome_arquivo}.mp3!")
+    except Exception as error:
+        print(f"[!] Ocorreu um erro: {error}")
         return
-    print('Digite o nome do arquivo de áudio que deseja salvar (sem extensão):')
-    nome_arquivo = input()
-    print (f"{path}")
-    # print (texto) # Para debug
-    comunicador = edge_tts.Communicate(
-        text=texto,
-        voice="pt-BR-AntonioNeural",  # Ou "pt-BR-FranciscaNeural"
-        rate="+5%",  # velocidade: -100% a +100%
-    )
-    await comunicador.save(f"Resultados\\{nome_arquivo}.mp3")
-    print(f"Arquivo de áudio gerado com sucesso em {os.getcwd()}\\Resultados\\{nome_arquivo}.mp3!")
 
 
 def loadFile(path):
